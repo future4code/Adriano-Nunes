06fc18a9ae3da15b372226707f10551921ca981f
adrianobnunes <adrianobn.biologia@gmail.com>
2021-10-05 20:34:14 -0300

    realização da questão 6

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -44,13 +44,26 @@ function retornaMaiorNumero(array) {
 
 // EXERCÍCIO 05
 function retornaQuantidadeElementos(array) {
-  let outroArray = array.length
-  return outroArray
+  let outroArray = array.length;
+  return outroArray;
 }
 
 // EXERCÍCIO 06
 function retornaExpressoesBooleanas() {
-
+  const booleano1 = true;
+  const booleano2 = false;
+  const booleano3 = !booleano2;
+  const booleano4 = !booleano3; 
+  
+  const expressao1 = booleano1 && booleano2 && !booleano4;
+  const expressao2 = (booleano1 && booleano2) || !booleano3;
+  const expressao3 = (booleano2 || booleano3) && (booleano4 || booleano1);
+  const expressao4 = !(booleano2 && booleano3) || !(booleano1 && booleano3);
+  const expressao5 = !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3);
+    
+  const respostas = [expressao1, expressao2, expressao3, expressao4, expressao5];
+  return respostas;
+  
 }
 
 // EXERCÍCIO 07
