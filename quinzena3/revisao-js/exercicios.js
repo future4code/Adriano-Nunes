0a8ba150706bc99d4dde8b8be8d788e195fa3dbb
adrianobnunes <adrianobn.biologia@gmail.com>
2021-10-07 16:47:31 -0300

    realização questão 13

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -142,6 +142,16 @@ function filmeFavorito() {
 
 // EXERCÍCIO 13s
 function imprimeChamada() {
+  
+  const astrodev = {
+    nome: "O Diabo Veste Prada",
+    ano: 2006,
+    diretor: "David Frankel",
+    atores: ["Meryl Streep", "Anne Hathaway", "Emily Blunt", "Stanley Tucci"]
+  }
+
+  return `Venha assistir ao filme ${astrodev.nome}, de ${astrodev.ano}, dirigido por ${astrodev.diretor} e estrelado por ${astrodev.atores[0]}, ${astrodev.atores[1]}, ${astrodev.atores[2]}, ${astrodev.atores[3]}.`
+  
   // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
 }
 
