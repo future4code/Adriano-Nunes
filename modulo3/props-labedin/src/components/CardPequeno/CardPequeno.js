85c4ce98804c14e3b9647e254a6cbe69c2386cbd
adrianobnunes <adrianobn.biologia@gmail.com>
2021-11-11 21:20:11 -0300

    Realização do style.components no CardPequeno.js

--- modulo3/props-labedin/src/components/CardPequeno/CardPequeno.js
+++ modulo3/props-labedin/src/components/CardPequeno/CardPequeno.js
@@ -1,15 +1,42 @@
 import React from 'react';
-import './CardPequeno.css'
+import styled from 'styled-components';
+
+const CardPequenoContainer = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid black;
+    padding: 20px 10px;
+    margin-bottom: 10px;
+    height: 100px;
+`
+
+const ImagemPequenoCard = styled.img`
+    width: 70px;
+    margin-right: 10px;
+    border-radius: 50%;
+`
+
+const ContainerPequenoCardInfo = styled.div`
+    display: flex;
+`
+
+const SubtituloPequenoCard = styled.h4`
+    margin-bottom: 15px;
+`
+
+const TextoPequenoCard = styled.p`
+    margin-left: 5px;
+`
 
 const CardPequeno = (props) => {
     return (
-        <div className="littlecard-container">
-            <img src= { props.imagem } />
-            <div>
-                <h4>{ props.nome }</h4>
-                <p>{ props.descricao }</p>
-            </div>
-        </div>
+        <CardPequenoContainer>
+            <ImagemPequenoCard src= { props.imagem } />
+            <ContainerPequenoCardInfo>
+                <SubtituloPequenoCard>{ props.nome }</SubtituloPequenoCard>
+                <TextoPequenoCard>{ props.descricao }</TextoPequenoCard>
+            </ContainerPequenoCardInfo>
+        </CardPequenoContainer>
     )
 }
 
