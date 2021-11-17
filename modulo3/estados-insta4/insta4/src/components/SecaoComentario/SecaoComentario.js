8630529b42db44fc5404bdd93c2251293d17a7bf
adrianobnunes <adrianobn.biologia@gmail.com>
2021-11-17 20:20:21 -0300

    alguns ajustes no componente post

--- modulo3/estados-insta4/insta4/src/components/SecaoComentario/SecaoComentario.js
+++ modulo3/estados-insta4/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -14,18 +14,18 @@ const InputComentario = styled.input`
 
 export class SecaoComentario extends Component {
 	state = {
-
+		comentario: ''
 	}
 
-	onChangeComentario() {
-
+	onChangeComentario = (event) => {
+		this.setState({comentario: event.target.value})
 	}
 
 	render() {
 		return <CommentContainer>
 			<InputComentario
 				placeholder={'Comentário'}
-				value={''}
+				value={this.state.comentario}
 				onChange={this.onChangeComentario}
 			/>
 			<button onClick={this.props.aoEnviar}>Enviar</button>
