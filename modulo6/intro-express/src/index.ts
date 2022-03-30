9736c1bb1e14aa5a8bc0c45a75eeb464e6465cc8
adrianobnunes <adrianobn.biologia@gmail.com>
2022-03-29 22:05:58 -0300

    Exercicios 2 e 3 ok

--- modulo6/intro-express/src/index.ts
+++ modulo6/intro-express/src/index.ts
@@ -12,7 +12,40 @@ app.listen(3003, () => {
 });
 
 //Exercício 1
-app.get("/", (request: Request, response: Response) => {    
-  console.log("Get ok")      
-  response.send("O Grêmio não tem Mundial, nem o Coritiba, nem o Galo!")
-});
+// app.get("/", (request: Request, response: Response) => {    
+//   console.log("Get ok")      
+//   response.send("O Grêmio não tem Mundial, nem o Coritiba, nem o Galo!")
+// });
+
+//Exercício 2
+type Users = {
+  id: string,
+  name: string,
+  phone: number, 
+  email: string,
+  website: string
+}
+
+const listUser: Users[] = [
+  {
+    id: "1a2b",
+    name: "Adriano",
+    phone: 999999999, 
+    email: "adriano@email.com",
+    website: "www.adrianobnunes.com.br"
+  }, 
+  {
+    id: "2e3n",
+    name: "Keianne",
+    phone: 988889889, 
+    email: "keianne@email.com",
+    website: "www.keiannedzaze.edu.br"
+  },
+  {
+    id: "5y8d",
+    name: "João Silencioso",
+    phone: 985568779, 
+    email: "joaosilencioso@email.com",
+    website: "www.joaosilencioso.ong.br"
+  }
+] 
