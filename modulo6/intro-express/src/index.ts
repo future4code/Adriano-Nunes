3dfcb9a6bb3bacf303bf0dc17666bf1f1a1742b9
adrianobnunes <adrianobn.biologia@gmail.com>
2022-03-29 22:31:21 -0300

    Exercicios 5 e 6 ok

--- modulo6/intro-express/src/index.ts
+++ modulo6/intro-express/src/index.ts
@@ -23,37 +23,100 @@ type Users = {
   name: string,
   phone: number, 
   email: string,
-  website: string
+  website: string,
+  post: Post[]
 }
 
 //Exercício 3
-const listUser: Users[] = [
-  {
-    id: "1a2b",
-    name: "Adriano",
-    phone: 999999999, 
-    email: "adriano@email.com",
-    website: "www.adrianobnunes.com.br"
-  }, 
-  {
-    id: "2e3n",
-    name: "Keianne",
-    phone: 988889889, 
-    email: "keianne@email.com",
-    website: "www.keiannedzaze.edu.br"
-  },
-  {
-    id: "5y8d",
-    name: "João Silencioso",
-    phone: 985568779, 
-    email: "joaosilencioso@email.com",
-    website: "www.joaosilencioso.ong.br"
-  }
-] 
+// const listUser: Users[] = [
+//   {
+//     id: "1a2b",
+//     name: "Adriano",
+//     phone: 999999999, 
+//     email: "adriano@email.com",
+//     website: "www.adrianobnunes.com.br"
+//   }, 
+//   {
+//     id: "2e3n",
+//     name: "Keianne",
+//     phone: 988889889, 
+//     email: "keianne@email.com",
+//     website: "www.keiannedzaze.edu.br"
+//   },
+//   {
+//     id: "5y8d",
+//     name: "João Silencioso",
+//     phone: 985568779, 
+//     email: "joaosilencioso@email.com",
+//     website: "www.joaosilencioso.ong.br"
+//   }
+// ] 
 
 //Exercício 4
 app.get("/users", (req: Request, res:Response) => {
     const users = listUser.map((user) => user);
 
     res.send(users);
+})
+
+//Exercício 5
+type Post = {
+  id: number,
+  title: string,
+  body: string,
+  userId: Users["id"]
+}
+
+const listUser: Users[] = [
+  {
+    id: "1a2b",
+    name: "Adriano",
+    phone: 999999999, 
+    email: "adriano@email.com",
+    website: "www.adrianobnunes.com.br",
+    post: [
+      {
+        id: 1,
+        title: "Internacional",
+        body: "Campeão de Tudo!",
+        userId: "1a2b"
+      }
+    ]
+  }, 
+  {
+    id: "2e3n",
+    name: "Keianne",
+    phone: 988889889, 
+    email: "keianne@email.com",
+    website: "www.keiannedzaze.edu.br",
+    post: [
+      {
+        id: 1,
+        title: "Pedagoga",
+        body: "Melhores dicas e aulas para Educação Infantil",
+        userId: "2e3n"
+      }
+    ]
+  },
+  {
+    id: "5y8d",
+    name: "João Silencioso",
+    phone: 985568779, 
+    email: "joaosilencioso@email.com",
+    website: "www.joaosilencioso.ong.br",
+    post: [
+      {
+        id: 1,
+        title: "Churrasco",
+        body: "A comida preferida de todos os gaúchos",
+        userId: "5y8d"
+      }
+    ]
+  }
+]
+
+app.get("/users/post", (req: Request, res:Response) => {
+  const posts = listUser.map((post) => post);
+
+  res.send(posts);
 })
