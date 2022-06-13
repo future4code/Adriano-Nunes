b41096bb9fce274f78b1d4bc1fd247db77406b38
adrianobnunes <adrianobn.biologia@gmail.com>
2022-06-12 23:51:03 -0300

    comecei a organizar os endpoints

--- modulo8/LaBook/src/index.ts
+++ modulo8/LaBook/src/index.ts
@@ -2,57 +2,16 @@
 
 import { Request, Response } from "express"
 
-import knex from "knex"
-import dotenv from "dotenv"
 
-import Knex from "knex"
 import { generateId } from "./services/generateID"
 import { app } from "./controller/app"
-
-/**************************** CONFIG ******************************/
-
-dotenv.config()
-
-export const connection: Knex = knex({
-   client: "mysql",
-   connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_SCHEMA,
-      port: 3306,
-      multipleStatements: true
-   }
-})
-
 
 
 /**************************** TYPES ******************************/
 
-type authenticationData = {
-   id: string
-}
 
-type user = {
-   id: string,
-   name: string,
-   email: string,
-   password: string
-}
 
-enum POST_TYPES {
-   NORMAL = "normal",
-   EVENT = "event"
-}
 
-type post = {
-   id: string,
-   photo: string,
-   description: string,
-   type: POST_TYPES,
-   createdAt: Date,
-   authorId: string
-}
 
 
 /**************************** ENDPOINTS ******************************/
