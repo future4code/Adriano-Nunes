be86eadd7b52c4d0d520118421e931689e54b478
adrianobnunes <adrianobn.biologia@gmail.com>
2022-06-24 23:53:45 -0300

    ajustes da pasta model

--- modulo8/Cookenu/src/index.ts
+++ modulo8/Cookenu/src/index.ts
@@ -1,18 +1,13 @@
 //import app from "./controller/app"
 
-import express, { Express } from "express";
-import cors from "cors";
-import { AddressInfo } from "net";
+import express from "express"
+import cors from "cors"
 
-export const app: Express = express();
-app.use(express.json());
-app.use(cors());
+export const app = express()
 
-const server = app.listen(process.env.PORT || 3003, () => {
-    if (server) {
-       const address = server.address() as AddressInfo;
-       console.log(`Server is running in http://localhost: ${address.port}`);
-    } else {
-       console.error(`Failure upon starting server.`);
-    }
-});
+app.use(express.json())
+app.use(cors())
+
+app.listen(3003, () => {
+    console.log("Servidor pronto na porta 3003")
+}) 
